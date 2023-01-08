bb8da81d2ed3f0a623c5b8432090396ce47dfdac
Ehab-Iqnaibi <iqnaibi_ehab@ppu.edu>
2023-01-08 23:53:38 +0200

    get_student_grades

--- Course/main.py
+++ Course/main.py
@@ -8,30 +8,39 @@ import numpy as np
 
 course_data = pd.read_csv("course.csv")
 #print(course_data)
-def course_activities():
 
 class students:
 
     def __init__(self, data):
-        self.df = data.iloc[:, 2:]
-    def student_grades(self):
-        # Iterate through each row in the DataFrame
-        for index, row in self.df.iterrows():
-            print(index)
-            print(row)
-            # Get the name and marks for the student
-            name = row["Name"]
-            reports = row["Reports"]
-            hw = row["HW"]
-            midterm = row["Midterm"]
-            practical = row["Practical"]
-            final = row["Final"]
-            total_grade = row["Total Grade"]
-            std_grades=[name,reports,hw,midterm,practical,final,total_grade]
-            course_activities(std_grades)
+        self.df = data
+    def get_student_grades(self,std_name,std_grade):
+        print(std_name)
+        print(std_grade)
+
+   # def course_activities(self):
 
 
+std=students(course_data)
+# Iterate through each row in the DataFrame
+df = course_data.set_index('Name')
+df = df.iloc[:, 2:]
+index=0
+for i, row in df.iterrows():
+    student_name = i
+    student_marks = row
+    # Get the name and marks for the student
+    #name = row["Name"]
+    reports = row["Reports"]
+    hw = row["HW"]
+    midterm = row["Midterm"]
+    practical = row["Practical"]
+    final = row["Final"]
+    total_grade = row["Total Grade"]
+    std_grades = [ reports, hw, midterm, practical, final, total_grade]
+    if index == 0:
+        weight_lis = std_grades
+        #print(weight_lis)
+    else:
+        std.get_student_grades(student_name,std_grades)
+    index = index+1
 
-
-
-students(course_data)
--- Course/test.py
+++ Course/test.py
@@ -38,6 +38,29 @@ std_names = df['Name']
 print(df)
 print(df.iloc[7].isnull()['HW'] )
 print(std_names)
+# *******************************
+    def course_activities(self,std_grade):
+        print(std_grade)
+
+    def student_grades(self):
+        # Iterate through each row in the DataFrame
+        for index, row in self.df.iterrows():
+            print(index)
+            print(row)
+
+            # Get the name and marks for the student
+            name = row["Name"]
+            reports = row["Reports"]
+            hw = row["HW"]
+            midterm = row["Midterm"]
+            practical = row["Practical"]
+            final = row["Final"]
+            total_grade = row["Total Grade"]
+            std_grades=[name,reports,hw,midterm,practical,final,total_grade]
+            if index==0:
+                weight_lis=std_grades
+            else:
+            self.course_activities(std_grades)
 '''
 
 #marks_df = pd.read_excel("Marks\Class1_Marks.xlsx")
@@ -50,9 +73,18 @@ for i, row in df.iterrows():
     student_name = i
     student_marks = row
 
+    # Check if any of the marks is null
+    if student_marks.isnull().any():
+        # Print the name of the student and the name of the column that is null
+        print(f"{student_name}: {student_marks.name[student_marks.isnull()]} is null")
+
     # Print the student name and marks
     print(student_name)
     print(student_marks)
+    print(student_marks.loc["Total Grade"])
+    print(student_marks.iloc[2])
+
+
 
 
 
